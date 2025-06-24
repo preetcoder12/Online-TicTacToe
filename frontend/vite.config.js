1d945eeafc9f3c6ff5b28b47f13272f0f5a872bc
preetcoder12 <preetgusainn23@gmail.com>
2025-06-24 23:00:54 +0530

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,3 +6,12 @@ export default defineConfig({
   plugins: [react()],
   base: './', // ✅ important for static hosting on Render
 });
+=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+>>>>>>> 1ad0c7d (new)
